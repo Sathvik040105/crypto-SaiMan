ef1c10390743883c1c8e62a9c79678eda95c3505
Sathvik040105 <sathvikm@iisc.ac.in>
2023-07-23 23:10:19 +0530

    deleted:    github.jpeg 	new file:   github2.png 	modified:   index.html 	deleted:    linkedin.jpg 	new file:   linkedin.png 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -754,32 +754,3 @@ function opensubTab() {
 }
 document.getElementById("defaultOpen3").onselect()
 
-  
-
-let linkedinsathvButton = document.getElementById("linkedin-button");
-linkedinsathvButton.addEventListener("click", website);
-  
-let linkedinsaiButton = document.getElementById("linkedin-button1");
-linkedinsaiButton.addEventListener("click", website1);
-
-function website() {
-    window.open("https://www.linkedin.com/in/sathvik-manthri-365984259/");
-}
-
-function website1() {
-    window.open("https://www.linkedin.com/in/nagasai-jajapuram-1b8a73273/");
-}
-
-let gitsathvButton = document.getElementById("git-button");
-gitsathvButton.addEventListener("click", website2);
-  
-let gitsaiButton = document.getElementById("git-button1");
-gitsaiButton.addEventListener("click", website3);
-
-function website2() {
-    window.open("https://github.com/Sathvik040105/");
-}
-
-function website3() {
-    window.open("https://github.com/Nagasai561/");
-}
